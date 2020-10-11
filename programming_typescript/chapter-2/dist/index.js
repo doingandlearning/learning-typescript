c05142ef7bdc4ebb3369e4f93f23f87e257ff611
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-10-11 19:53:38 +0100

    Initial commit.

---
+++ programming_typescript/chapter-2/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello Typescript!");
+let a = 1 + "a" + [true];
+let b = a + 3;
+let c = { apple: a, banana: b };
+let d = c.apple * 4;
+//# sourceMappingURL=index.js.map
---
+++ programming_typescript/chapter-2/src/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello Typescript!");
+
+let a = 1 + "a" + [true];
+let b = a + 3;
+let c = { apple: a, banana: b };
+let d = c.apple * 4;
